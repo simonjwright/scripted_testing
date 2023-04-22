9d0890ae989764dc72692dd0b611c2b8a144b3f0
Simon Wright <simon@pushface.org>
2023-04-22 15:18:05 +0100

    demo: extend period allowed for the application to catch up.
      * demo/demo.tcl (PAUSE): new, set to 1 ms. Changed the previous
          waits (for 1 us) to 1 ms.

--- demo/demo.tcl
+++ demo/demo.tcl
@@ -1,35 +1,36 @@
 # Copyright (C) 2023 Simon Wright <simon@pushface.org>
 # SPDX-License-Identifier: CC0-1.0
 
-echo "wait 1"
+# We have to give the application time to accept inputs; waiting
+# (delaying) means that the application task can run.
+set PAUSE 0.001
+
 wait 1.0
 echo "setting {0, false}"
 callback-digital_io.input_signal_state {0 false}
-wait 0.000_001
+wait $PAUSE
 check_number_of_calls digital_io.set 1
 check-boolean-for-digital_io.output_signal \
     digital_io.set \
     o 0 \
     to_state true
 
-echo "wait 1"
 wait 1.0
 echo "setting {1, true}"
 save_number_of_calls digital_io.set
 callback-digital_io.input_signal_state {1 true}
-wait 0.000_001
+wait $PAUSE
 check_number_of_new_calls digital_io.set 1
 check-boolean-for-digital_io.output_signal \
     digital_io.set \
     o 1 \
     to_state false
 
-echo "wait 1"
 wait 1.0
 echo "setting {15, false}"
 save_number_of_calls digital_io.set
 callback-digital_io.input_signal_state {15 false}
-wait 0.000_001
+wait $PAUSE
 check_number_of_new_calls digital_io.set 1
 check-boolean-for-digital_io.output_signal \
     digital_io.set \
